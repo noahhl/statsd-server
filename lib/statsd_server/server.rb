ae7948bfdfef5b3c0123a5d3a6daca12c93c71f1
Noah Lorang <noah@datarobots.com>
2011-12-01 20:59:36 -0500

    Clean up the main server loop
    Remove unused constant, add comments

--- lib/statsd_server/server.rb
+++ lib/statsd_server/server.rb
@@ -5,12 +5,11 @@ require 'erb'
 module StatsdServer
   module Server #< EM::Connection  
     
-    FLUSH_INTERVAL = 10
     COUNTERS = {}
     TIMERS = {}
 
     def post_init
-      puts "statsd server started!"
+      puts "#{Time.now} statsd server started!"
     end
 
     def self.get_and_clear_stats!
@@ -56,26 +55,31 @@ module StatsdServer
 
         # Start the server
         EventMachine::run do
+          #Bind to the socket and gather the incoming datapoints
           EventMachine::open_datagram_socket(config['bind'], config['port'], StatsdServer::Server)  
           
-          # Periodically Flush
+          #On the flush interval, do the primary aggregation and flush it to
+          #a redis zset
           EventMachine::add_periodic_timer(config['flush_interval']) do
             counters,timers = StatsdServer::Server.get_and_clear_stats!
-              EM.defer { StatsdServer::RedisStore.flush_stats(counters,timers) } 
-
+            EM.defer { StatsdServer::RedisStore.flush_stats(counters,timers) } 
           end
 
-          #Clean up redis zsets
-              EventMachine::add_periodic_timer(config['flush_interval'] * 200) do
-                EM.defer {StatsdServer::RedisStore.cleanup }
+          #At every retention that's longer than the flush interval, 
+          #perform an aggregation and store it to disk
+          StatsdServer::RedisStore.retentions.each do |retention|
+            unless retention.split(":")[0].to_i == config["flush_interval"] 
+              EventMachine::add_periodic_timer(retention.split(":")[0].to_i) do
+                EM.defer {StatsdServer::RedisStore.aggregate(retention)}
               end
-              StatsdServer::RedisStore.retentions.each_with_index do |retention, index|
-                unless index.zero?
-                  EventMachine::add_periodic_timer(retention.split(":")[0].to_i) do
-                    EM.defer {StatsdServer::RedisStore.aggregate(retention)}
-                  end
-                end
             end
+          end
+
+          #Every n flush intervals, clean up those values that are past their
+          #retention limit
+          EventMachine::add_periodic_timer(config['flush_interval'] * 200) do
+            EM.defer {StatsdServer::RedisStore.cleanup }
+          end
 
         end
       end
